7484b7e1a199ab66b0c53b78e7df6865f9610af1
Sameer Srivastava <112320710+samerr07@users.noreply.github.com>
2024-07-13 17:43:23 +0530

    Update config.js

--- client/src/utility/config.js
+++ client/src/utility/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://chat-app-mern-swart.vercel.app/api/v1"
+export const BASE_URL = "https://chat-app-mern-two-eosin.vercel.app/api/v1"
 
 
 export const timeSince = (timestamp) => {

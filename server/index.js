61edf93ad4a90c7d3d921bd34dc095271dc62995
Sameer Srivastava <112320710+samerr07@users.noreply.github.com>
2024-07-13 17:54:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,51 +1,104 @@
+// const express = require('express');
+// const cors = require('cors');
+// const cookieParser = require('cookie-parser');
+// const dotenv = require("dotenv");
+// const connectDatabase  = require('./config/database');
+// const userRouter = require("./routes/userRoutes")
+// const messageRouter = require("./routes/messageRoute")
+// const {server, socketServer} = require("./socket/socket")
+
+
+
+// dotenv.config()
+
+
+// //crete a server
+// // const server = express();
+
+// //connecting a database
+// connectDatabase()
+
+// //middlewares
+
+// const corsOption={
+//     origin: true,
+//     credentials:true
+// };
+// server.use(cors(corsOption));
+// server.use(express.json());
+// server.use(express.urlencoded({extended: true}));
+// server.use(cookieParser())
+
+
+
+// //Routes
+// server.use("/api/v1/user",userRouter.router)
+// server.use("/api/v1/message",messageRouter.router)
+
+
+// server.get("/",(req,res)=>{
+//     res.send("Hello World")
+// })
+
+// //listening on port
+
+// // server.listen(process.env.PORT, ()=>{
+// //     console.log("Server is started !!!")
+// // })
+
+// socketServer.listen(process.env.PORT, ()=>{
+//     console.log("Server is started !!!")
+// })
+
+
+
+
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const dotenv = require("dotenv");
-const connectDatabase  = require('./config/database');
-const userRouter = require("./routes/userRoutes")
-const messageRouter = require("./routes/messageRoute")
-const {server, socketServer} = require("./socket/socket")
+const connectDatabase = require('./config/database');
+const userRouter = require("./routes/userRoutes");
+const messageRouter = require("./routes/messageRoute");
+const { server, socketServer } = require("./socket/socket");
 
+dotenv.config();
 
+// Connect to the database
+connectDatabase();
 
-dotenv.config()
+// Middleware configurations
+const allowedOrigins = [
+    'https://chat-app-mern-si4h.vercel.app',
+    'https://chat-app-mern-two-eosin.vercel.app'
+];
 
-
-//crete a server
-// const server = express();
-
-//connecting a database
-connectDatabase()
-
-//middlewares
-
-const corsOption={
-    origin: true,
-    credentials:true
+const corsOptions = {
+    origin: (origin, callback) => {
+        console.log("Origin: ", origin);  // Add logging to help debug
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
 };
-server.use(cors(corsOption));
+
+server.use(cors(corsOptions));
 server.use(express.json());
-server.use(express.urlencoded({extended: true}));
-server.use(cookieParser())
+server.use(express.urlencoded({ extended: true }));
+server.use(cookieParser());
 
+// Routes
+server.use("/api/v1/user", userRouter.router);
+server.use("/api/v1/message", messageRouter.router);
 
+server.get("/", (req, res) => {
+    res.send("Hello World");
+});
 
-//Routes
-server.use("/api/v1/user",userRouter.router)
-server.use("/api/v1/message",messageRouter.router)
-
-
-server.get("/",(req,res)=>{
-    res.send("Hello World")
-})
-
-//listening on port
-
-// server.listen(process.env.PORT, ()=>{
-//     console.log("Server is started !!!")
-// })
-
-socketServer.listen(process.env.PORT, ()=>{
-    console.log("Server is started !!!")
-})
+// Start the server
+socketServer.listen(process.env.PORT, () => {
+    console.log("Server is started on port", process.env.PORT);
+});

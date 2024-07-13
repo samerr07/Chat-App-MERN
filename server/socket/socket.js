d1695d177753da028adc126aa8b7b4ca16994aed
Sameer Srivastava <112320710+samerr07@users.noreply.github.com>
2024-07-13 17:39:35 +0530

    Update socket.js

--- server/socket/socket.js
+++ server/socket/socket.js
@@ -9,7 +9,7 @@ const socketServer = http.createServer(server);
 
 const io = new Server(socketServer,{
     cors:{
-        origin:['https://chat-app-mern-ulg2.vercel.app/'],
+        origin:true,
         methods:['GET', 'POST'],
     }
 })
